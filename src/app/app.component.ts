690283fd234f38fa8c905b93b8f575ca23242c5f
vchugunov <vchugunov@digimatics.ru>
2020-11-22 00:09:07 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,117 @@
 import { Component } from '@angular/core';
 
+class Noise {
+  static audioContext = new window.AudioContext;
+  static fadeOutTimer: any;
+
+  // https://noisehack.com/generate-noise-web-audio-api/
+  static createNoise(track: any) {
+
+    const bufferSize = 2 * Noise.audioContext.sampleRate;
+    const noiseBuffer = Noise.audioContext.createBuffer(1, bufferSize, Noise.audioContext.sampleRate);
+    const output = noiseBuffer.getChannelData(0);
+
+    for (let i = 0; i < bufferSize; i++) {
+      output[i] = Math.random() * 2 - 1;
+    }
+
+    track.audioSource.buffer = noiseBuffer;
+  }
+
+  static stopNoise(track: any) {
+    if (track.audioSource) {
+      clearTimeout(Noise.fadeOutTimer);
+      track.audioSource.stop();
+    }
+  }
+
+  static fadeNoise(track: any) {
+
+    if (track.fadeOut) {
+      track.fadeOut = (track.fadeOut >= 0) ? track.fadeOut : 0.5;
+    } else {
+      track.fadeOut = 0.5;
+    }
+
+    if (track.canFade) {
+      track.gainNode.gain.linearRampToValueAtTime(0, Noise.audioContext.currentTime + track.fadeOut);
+      track.canFade = false;
+
+      Noise.fadeOutTimer = setTimeout(() => {
+        Noise.stopNoise(track);
+      }, track.fadeOut * 1000);
+
+    } else {
+      Noise.stopNoise(track);
+    }
+  }
+
+  static buildTrack(track: any) {
+    track.audioSource = Noise.audioContext.createBufferSource();
+    track.gainNode = Noise.audioContext.createGain();
+    track.audioSource.connect(track.gainNode);
+    track.gainNode.connect(Noise.audioContext.destination);
+    track.canFade = true; // used to prevent fadeOut firing twice
+  }
+
+  static setGain(track: any) {
+
+    track.volume = (track.volume >= 0) ? track.volume : 0.5;
+
+    if (track.fadeIn) {
+      track.fadeIn = (track.fadeIn >= 0) ? track.fadeIn : 0.5;
+    } else {
+      track.fadeIn = 0.5;
+    }
+
+    track.gainNode.gain.setValueAtTime(0, Noise.audioContext.currentTime);
+
+    track.gainNode.gain.linearRampToValueAtTime(track.volume / 4, Noise.audioContext.currentTime + track.fadeIn / 2);
+
+    track.gainNode.gain.linearRampToValueAtTime(track.volume, Noise.audioContext.currentTime + track.fadeIn);
+
+  }
+
+  static playNoise(track: any) {
+    Noise.stopNoise(track);
+    Noise.buildTrack(track);
+    Noise.createNoise(track);
+    Noise.setGain(track);
+    track.audioSource.loop = true;
+    track.audioSource.start();
+  }
+}
+
+var noise = {
+  volume: 0.05, // 0 - 1
+  fadeIn: 2.5, // time in seconds
+  fadeOut: 1.3, // time in seconds
+}
+
+
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" (click)="perform()">
+    <rect *ngIf="play; else elseBlock" fill="#000000" x="2" y="2" width="6" height="6"></rect>  
+    <ng-template #elseBlock>
+      <path fill="#000000" d="M 2,2 L 8,5 2,8 Z"></path>
+    </ng-template>
+  </svg>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'white-noise';
+  title = 'White Noise';
+
+  play = false
+
+  perform() {
+    if (this.play) {
+      Noise.stopNoise(noise)
+    } else {
+      Noise.playNoise(noise)
+    }
+    this.play = !this.play;
+  }
 }
